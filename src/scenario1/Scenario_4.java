40580d25a9b197f416a5b85a7d2c4de8e0ecca64
praspeshve <praspeshve@gmail.com>
2019-04-24 16:08:08 +0530

    scen

--- src/scenario1/Scenario_4.java
+++ src/scenario1/Scenario_4.java
@@ -1,5 +1,15 @@
 package scenario1;
 
-public class Scenario_4 {
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
 
+public class Scenario_4  implements Constant{
+	public static void main(String[] args) {
+		System.setProperty(GECKO_KEY, GECKO_VALUE);
+		WebDriver driver = new FirefoxDriver();;
+		driver.get(url);
+		System.out.println("abc");
+		
+	}
+	
 }
